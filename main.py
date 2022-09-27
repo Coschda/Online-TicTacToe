5eac96743126536edd67cf8755ac66bf96d0c471
Coschda <mikail.doumi@gmail.com>
2022-09-27 16:20:03 +0200

    simple pygame white window

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+
+WIDTH, HEIGHT = 500, 500
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("TicTacToe")
+
+def draw(win):
+    win.fill((255,255,255))
+
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+
+    draw(win)
+
+main()
