ab6682943c4813c6487997a96d7afed0935a8652
Coschda <mikail.doumi@gmail.com>
2022-09-27 16:27:53 +0200

    Drawing the grid

--- main.py
+++ main.py
@@ -5,7 +5,17 @@ win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("TicTacToe")
 
 def draw(win):
+    #Background
     win.fill((255,255,255))
+
+    #Grid
+    thickness = 8
+    pygame.draw.line(win, (0,0,0), (WIDTH/3,0), (WIDTH/3, HEIGHT), thickness)
+    pygame.draw.line(win, (0,0,0), (2*WIDTH/3,0), (2*WIDTH/3, HEIGHT), thickness)
+
+    pygame.draw.line(win, (0,0,0), (0,WIDTH/3), (HEIGHT, WIDTH/3), thickness)
+    pygame.draw.line(win, (0,0,0), (0,2*WIDTH/3), (HEIGHT, 2*WIDTH/3), thickness)
+
     pygame.display.update()
 
 def main():
